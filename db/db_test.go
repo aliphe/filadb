0db170dadbdd0b37fd5abf27088784973d13d83a
aliphe <matthias.alif@gmail.com>
2024-03-24 22:39:13 +0100

    feat: super basic append only log implementation

---
+++ db/db_test.go
@@ -0,0 +1,91 @@
+package db
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func Test_Set(t *testing.T) {
+
+	type row struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	}
+
+	tests := []struct {
+		ins row
+	}{
+		{
+			ins: row{
+				ID:   "1",
+				Name: "tust",
+			},
+		},
+	}
+
+	f, err := os.OpenFile("db.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		t.Errorf("open db file: %v", err.Error())
+	}
+	db := New(f)
+
+	for _, tc := range tests {
+		err := db.Set(context.Background(), tc.ins)
+		if err != nil {
+			t.Fatal(err)
+		}
+		r, f, err := db.Get(context.Background(), tc.ins.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if f != true {
+			t.Fatal("inserted row now found")
+		}
+		var ins row
+		err = json.Unmarshal(r, &ins)
+		if err != nil {
+			t.Fatal("invalid inserted row")
+		}
+		t.Logf("row: %v", ins)
+	}
+}
+
+func Test_split(t *testing.T) {
+	tests := []struct {
+		in   []byte
+		out  [][]byte
+		rest []byte
+	}{
+		{
+			in:   []byte("test"),
+			out:  nil,
+			rest: []byte("test"),
+		},
+		{
+			in:   []byte("test\ntust\n"),
+			out:  [][]byte{[]byte("test"), []byte("tust")},
+			rest: nil,
+		},
+	}
+
+	for idx, tc := range tests {
+		t.Run(fmt.Sprintf("%d", idx), func(t *testing.T) {
+			o, rest := split(tc.in, '\n')
+
+			if len(o) != len(tc.out) {
+				t.Errorf("lines: want %d, got %d", len(tc.out), len(o))
+			}
+			if string(rest) != string(tc.rest) {
+				t.Errorf("rest: want %s, got %s", string(tc.rest), string(rest))
+			}
+			for i := 0; i < len(o); i++ {
+				if string(o[i]) != string(tc.out[i]) {
+					t.Errorf("rest: want %s, got %s", string(tc.out[i]), string(o[i]))
+				}
+			}
+		})
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aliphe/filadb
+
+go 1.22.0
