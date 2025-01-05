46ec66d45fd1a243034b88214410823206d593bb
aliphe <matthias.alif@gmail.com>
2025-01-05 22:17:49 +0100

    feat(sql): prepare tests for plan part

--- query/sql/eval/plan_test.go
+++ query/sql/eval/plan_test.go
@@ -10,46 +10,59 @@ func Test_eval(t *testing.T) {
 	// select * from users
 	// join posts on posts.user_id = users.id
 	// where users.age > 21
-	var _ = step{
-		queries: []query{
-			{
-				table: "users",
-				filters: []parser.Filter{
-					{
-						Left: parser.Value{
-							Type: parser.ValueTypeReference,
-							Reference: parser.Field{
-								Column: "age",
-							},
-						},
-						Op: parser.OpMoreThan,
-						Right: parser.Value{
-							Type:  parser.ValueTypeLitteral,
-							Value: 21,
-						},
-					},
-				},
-			},
-		},
-		children: []step{
-			{
+	tests := []struct {
+		given string
+		want  step
+	}{
+		{
+			given: `
+			select * from users
+			join posts on posts.user_id = users.id
+			where users.age > 21
+			`,
+			want: step{
 				queries: []query{
 					{
-						table: "posts",
+						table: "users",
 						filters: []parser.Filter{
 							{
 								Left: parser.Value{
 									Type: parser.ValueTypeReference,
 									Reference: parser.Field{
-										Column: "user_id",
+										Column: "age",
 									},
 								},
-								Op: parser.OpEqual,
+								Op: parser.OpMoreThan,
 								Right: parser.Value{
-									Type: parser.ValueTypeReference,
-									Reference: parser.Field{
-										Table:  "users",
-										Column: "id",
+									Type:  parser.ValueTypeLitteral,
+									Value: 21,
+								},
+							},
+						},
+					},
+				},
+				children: []step{
+					{
+						queries: []query{
+							{
+								table: "posts",
+								filters: []parser.Filter{
+									{
+										Left: parser.Value{
+											Type: parser.ValueTypeReference,
+											Reference: parser.Field{
+												Table:  "posts",
+												Column: "user_id",
+											},
+										},
+										Op: parser.OpEqual,
+										Right: parser.Value{
+											Type: parser.ValueTypeReference,
+											Reference: parser.Field{
+												Table:  "users",
+												Column: "id",
+											},
+										},
 									},
 								},
 							},
@@ -60,34 +73,9 @@ func Test_eval(t *testing.T) {
 		},
 	}
 
-	// tests := []struct {
-	// 	given parser.Select
-	// 	want  step
-	// }{
-	// 	{
-	// 		given: parser.Select{
-	// 			From: parser.From{
-	// 				Table: "users",
-	// 				Where: []parser.Filter{
-	// 					{
-	// 						Field: parser.Field{
-	// 							Column: "age",
-	// 						},
-	// 						Op: parser.OpMoreThan,
-	// 						Value: parser.FilterValue{
-	// 							Type:  parser.FilterTypeLitteral,
-	// 							Value: 21,
-	// 						},
-	// 					},
-	// 				},
-	// 				Joins: []parser.Join{
-	// 					{
-	// 						Table:
-	// 					}
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// }
-
+	for _, tc := range tests {
+		t.Run(tc.given, func(_ *testing.T) {
+			return
+		})
+	}
 }
